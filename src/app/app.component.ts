d38f5c782d6fe546bfc7c5116dc8dd445f6e49b1
AnujAgrawal30 <anujagrawal@ANUJs-MacBook-Pro.local>
2022-03-17 00:14:36 +0530

    static first draft

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'lenz';
+  num_coils = 13;
+
+  ngAfterViewInit(): void {
+    var elem = document.getElementById("dragger");
+    if (elem){
+      this.dragElement(elem);
+    }
+  }
+
+  dragElement(elmnt: HTMLElement) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    elmnt.onmousedown = dragMouseDown;
+  
+    function dragMouseDown(e: MouseEvent) {
+      e = e || window.event;
+      e.preventDefault();
+      // get the mouse cursor position at startup:
+      pos3 = e.clientX;
+      // pos4 = e.clientY;
+      document.onmouseup = closeDragElement;
+      // call a function whenever the cursor moves:
+      document.onmousemove = elementDrag;
+    }
+  
+    function elementDrag(e: MouseEvent) {
+      e = e || window.event;
+      e.preventDefault();
+      // calculate the new cursor position:
+      pos1 = pos3 - e.clientX;
+      // pos2 = pos4 - e.clientY;
+      pos3 = e.clientX;
+      // pos4 = e.clientY;
+      // set the element's new position:
+      // elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+      elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+  
+    function closeDragElement() {
+      /* stop moving when mouse button is released:*/
+      document.onmouseup = null;
+      document.onmousemove = null;
+    }
+  }
 }
